f7ae6c693c81c4941dc2123cd0a4511b8b39fe16
Tyrail Leverett <tyrailleverett@gmail.com>
2023-02-04 14:15:19 -0600

    Added Zod

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import cors from "cors";
-import express, { type Request, type Response } from "express";
+import express, {
+    type NextFunction,
+    type Request,
+    type Response
+} from "express";
 import morgan from "morgan";
 
 const app = express();
@@ -9,5 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req: Request, res: Response) => res.sendStatus(404));
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(400).json({ err: err.message });
+});
 
 export default app;
